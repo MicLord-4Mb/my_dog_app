f390c0e59d44e1638d96778b22649ab474d21bae
MicLord <miclord@yahoo.com>
2026-09-12 12:02:54 +0300

    add root error boundary

--- src/routes/root.route.tsx
+++ src/routes/root.route.tsx
@@ -1,20 +1,18 @@
+import {RootErrorBoundary} from "@/components/layout/RootErrorBoundary";
 import {favoritesRoute} from "@/routes/favorites/favorites.route";
 import { galleryRoute } from "@/routes/gallery/gallery.route";
 import { homeRoute } from "@/routes/home/home.route";
 import type { RouteObject } from "react-router";
 import { Layout } from "@/components/layout/Layout";
-import { lazy } from "react";
 import { ROUTES } from "@/constants/routes";
 import { PageLoader } from "@/components/common/PageLoader";
 import { notFoundRoute } from "@/routes/not-found/not-found.route";
 
-const NotFoundPage = lazy(() => import('@/pages/NotFoundPage').then(m => ({ default: m.NotFoundPage })));
 
 export const rootRoute:RouteObject = {
   path: ROUTES.HOME,
   element: <Layout />,
-  //TODO: change that page
-  errorElement: <NotFoundPage />,
+  errorElement: <RootErrorBoundary />,
   HydrateFallback: PageLoader,
   children: [
     homeRoute,
