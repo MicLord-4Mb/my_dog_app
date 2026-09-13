4df418a316621cfdc0e4a4a596e3b258b36ecd02
MicLord <miclord@yahoo.com>
2026-09-13 15:00:38 +0300

    [fix] add redux logger in dev mode

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,28 @@
 import breedReducer from "@/features/breeds/breedSlice";
-import {configureStore} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import favoritesReducer from "@/features/favorites/favoritesSlice";
+import { createLogger } from 'redux-logger'
+
+const logger = createLogger({
+  collapsed: true,
+  duration: true,
+  diff: true,
+})
 
 export const store = configureStore({
   reducer: {
     breeds: breedReducer,
     favorites: favoritesReducer,
   },
+  middleware: (getDefaultMiddleware) => {
+    const middlewares = getDefaultMiddleware();
+
+    if (import.meta.env.DEV) {
+      return middlewares.concat(logger)
+    }
+
+    return middlewares;
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
