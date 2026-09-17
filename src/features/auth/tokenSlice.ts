b844ee22ffbc5067e7968e39ae4bf542c1954098
MicLord <miclord@yahoo.com>
2026-09-18 00:52:25 +0300

    [code] auth slices added

---
+++ src/features/auth/tokenSlice.ts
@@ -0,0 +1,36 @@
+import {getStoredAuthSession} from "@/api/authApi";
+import {loginThunk, logoutThunk} from "@/features/auth/authThunks";
+import type {TokenSliceState} from "@/types/auth.types";
+import {createSlice, type PayloadAction} from "@reduxjs/toolkit";
+
+const storedAuthSession = getStoredAuthSession();
+
+const initialState: TokenSliceState = {
+  token: storedAuthSession.token,
+};
+
+const tokenSlice = createSlice({
+  name: 'token',
+  initialState,
+  reducers: {
+    setToken: (state, action: PayloadAction<string | null>) => {
+      state.token = action.payload;
+    },
+    logout: (state) => {
+      state.token = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginThunk.fulfilled, (state, action) => {
+        state.token = action.payload.token;
+      })
+      .addCase(logoutThunk.fulfilled, (state) => {
+        state.token = null;
+      });
+
+  },
+});
+
+export const { setToken, logout } = tokenSlice.actions;
+export default tokenSlice.reducer;
---
+++ src/features/auth/userSlice.ts
@@ -0,0 +1,85 @@
+import {getStoredAuthSession} from "@/api/authApi";
+import {REQUEST_STATUS} from "@/constants/status";
+import {loginThunk, logoutThunk} from "@/features/auth/authThunks";
+import type {AuthUser, UserSliceState} from "@/types/auth.types";
+import {createSlice, type PayloadAction} from "@reduxjs/toolkit";
+import { logout as tokenLogoutAction } from "@/features/auth/tokenSlice"
+
+const storedAuthSession = getStoredAuthSession();
+
+const initialState: UserSliceState = {
+  request: storedAuthSession.user
+    ? { status: REQUEST_STATUS.SUCCESS, data: storedAuthSession.user, error: null }
+    : { status: REQUEST_STATUS.IDLE, data: null, error: null }
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<AuthUser | null>) => {
+      if (action.payload) {
+        state.request = {
+          status: REQUEST_STATUS.SUCCESS,
+          data: action.payload,
+          error: null,
+        };
+      } else {
+        state.request = {
+          status: REQUEST_STATUS.IDLE,
+          data: null,
+          error: null,
+        };
+      }
+    },
+    clearUser: (state) => {
+      state.request = {
+        status: REQUEST_STATUS.IDLE,
+        data: null,
+        error: null,
+      };
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginThunk.pending, (state) => {
+        state.request = {
+          status: REQUEST_STATUS.LOADING,
+          data: null,
+          error: null,
+        };
+      })
+      .addCase(loginThunk.fulfilled, (state, action ) => {
+        state.request = {
+          status: REQUEST_STATUS.SUCCESS,
+          data: action.payload.user,
+          error: null,
+        };
+      })
+      .addCase(loginThunk.rejected, (state, action) => {
+        state.request = {
+          status: REQUEST_STATUS.ERROR,
+          data: null,
+          error: action.payload
+            ?? { message: action.error.message ?? 'Login failed.' },
+        };
+      })
+      .addCase(logoutThunk.fulfilled, (state) => {
+        state.request = {
+          status: REQUEST_STATUS.IDLE,
+          data: null,
+          error: null,
+        };
+      })
+      .addCase(tokenLogoutAction, (state) => {
+        state.request = {
+          status: REQUEST_STATUS.IDLE,
+          data: null,
+          error: null,
+        };
+      });
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+export default userSlice.reducer;
